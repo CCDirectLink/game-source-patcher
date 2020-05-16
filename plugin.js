7263f53e9c9260cebeca80bf1a9d69d36300d8ec
ac2pic <ac2pic@gmail.com>
2020-05-16 14:54:41 -0500

    Basic mod stuff

---
+++ plugin.js
@@ -0,0 +1,18 @@
+export default class GameSourcePatcher extends Plugin {
+	constructor(mod) {
+		super();
+		this.mod = mod;
+	}
+
+	async preload() {
+
+	}
+
+	async postload() {
+
+	}
+
+	async prestart() {
+
+	}
+}
